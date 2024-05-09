4dfd0e22f3ef7dcfbe81cb3be469a432cc35ea27
pjpedersen <pjohnpedersen@gmail.com>
2024-05-09 23:30:05 +0200

    Changed asteroid spawn position to random width and height based on window size

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidControlSystem.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidControlSystem.java
@@ -17,15 +17,20 @@ public class AsteroidControlSystem implements IEntityProcessingService, Asteroid
         this.gameData = gameData;
         this.world = world;
         addAsteroid(gameData, world);
+        moveAsteroids();
     }
 
     @Override
     public Entity createAsteroid(GameData gameData) {
+        int sizeUnit = 10;
+        double randomX = getRandomInput(1, gameData.getDisplayWidth());
+        double randomY = getRandomInput(1, gameData.getDisplayHeight());
         Entity asteroid = new Asteroid();
         asteroid.setPolygonCoordinates(-5,-5,5,-5,5,5,-5,5);
-        asteroid.setX(gameData.getDisplayHeight()/3);
-        asteroid.setY(gameData.getDisplayWidth()/3);
+        asteroid.setX(randomX);
+        asteroid.setY(randomY);
         asteroid.setLifePoints(3);
+        asteroid.setRadius(sizeUnit);
         return asteroid;
     }
 
@@ -46,6 +51,35 @@ public class AsteroidControlSystem implements IEntityProcessingService, Asteroid
             world.addEntity(asteroid);
         }
     }
+
+    public void moveAsteroids() {
+
+        for (Entity e : world.getEntities(Asteroid.class)) {
+            if (e.getX() < 0) {
+                e.setX(1);
+            }
+
+            if (e.getX() > gameData.getDisplayWidth()) {
+                e.setX(gameData.getDisplayWidth()-1);
+            }
+
+            if (e.getY() < 0) {
+                e.setY(1);
+            }
+
+
+                    double changeX = Math.cos(Math.toRadians(e.getRotation()));
+                    double changeY = Math.sin(Math.toRadians(e.getRotation()));
+                    e.setX(e.getX() + changeX);
+                    e.setY(e.getY() + changeY);
+
+        }
+    }
+
+    public int getRandomInput(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
 
 
 }
