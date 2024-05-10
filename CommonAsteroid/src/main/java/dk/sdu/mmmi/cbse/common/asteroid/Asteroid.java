936f9d4f2b747dbe2944ad50ee70cf9cf63fefc6
pjpedersen <pjohnpedersen@gmail.com>
2024-05-10 19:23:20 +0200

    Added isSplit boolean to commonAsteroid Asteroid entity

--- CommonAsteroid/src/main/java/dk/sdu/mmmi/cbse/common/asteroid/Asteroid.java
+++ CommonAsteroid/src/main/java/dk/sdu/mmmi/cbse/common/asteroid/Asteroid.java
@@ -2,4 +2,14 @@ package dk.sdu.mmmi.cbse.common.asteroid;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 
 public class Asteroid extends Entity {
+
+    private boolean isSplit = false;
+
+    public boolean isSplit() {
+        return isSplit;
+    }
+
+    public void setSplit(boolean isSplit) {
+        this.isSplit = isSplit;
+    }
 }
