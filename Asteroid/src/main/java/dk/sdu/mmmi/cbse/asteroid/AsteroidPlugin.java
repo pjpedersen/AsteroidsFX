1e94c525d439b5a39bcb2d4884579353d3b2dece
pjpedersen <pjohnpedersen@gmail.com>
2024-05-12 21:47:08 +0200

    Refactored references to new CommonPlayer and CommonEnemy

--- Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
+++ Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
@@ -1,5 +1,7 @@
 package dk.sdu.mmmi.cbse.asteroid;
 
+import dk.sdu.mmmi.cbse.common.asteroid.CommonAsteroidEntity;
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
@@ -12,6 +14,10 @@ public class AsteroidPlugin implements IGamePluginService {
 
     @Override
     public void stop(GameData gameData, World world) {
-
+        for (Entity e : world.getEntities()) {
+            if (e.getClass() == CommonAsteroidEntity.class) {
+                world.removeEntity(e);
+            }
+        }
     }
 }
--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/Enemy.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/Enemy.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.cbse.enemy;
 import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.enemy.CommonEnemyEntity;
 
-public class Enemy extends Entity {
+public class Enemy extends CommonEnemyEntity {
 }
