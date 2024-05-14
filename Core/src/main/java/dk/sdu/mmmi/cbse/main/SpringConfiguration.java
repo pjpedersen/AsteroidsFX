2d6ecf1e57a2f577010440cd072681bcdbdf2e8b
pjpedersen <pjohnpedersen@gmail.com>
2024-05-14 03:00:44 +0200

    Created SpringConfiguration acting as config for the container with references to the relevant dependencies

---
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/SpringConfiguration.java
@@ -0,0 +1,38 @@
+package dk.sdu.mmmi.cbse.main;
+
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+import java.util.List;
+import java.util.ServiceLoader;
+import static java.util.stream.Collectors.toList;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+class SpringConfiguration {
+
+    public SpringConfiguration() {
+    }
+
+    @Bean
+    public GameLoop game(){
+        return new GameLoop(gamePluginServices(), entityProcessingServiceList(), postEntityProcessingServices());
+    }
+
+    @Bean
+    public List<IEntityProcessingService> entityProcessingServiceList(){
+        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    @Bean
+    public List<IGamePluginService> gamePluginServices() {
+        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+
+    @Bean
+    public List<IPostEntityProcessingService> postEntityProcessingServices() {
+        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+    }
+}
--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -1,8 +1,15 @@
 module Core {
+
+    //spring framework
+    requires spring.core;
+    requires spring.context;
+    requires spring.beans;
+
     requires Common;
     requires CommonBullet;    
-    requires javafx.graphics;    
-    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    requires javafx.graphics;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics, spring.core;
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
