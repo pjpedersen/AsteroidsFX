181500e0080bfad8649ead347b0a394e07f855aa
pjpedersen <pjohnpedersen@gmail.com>
2024-05-19 17:36:25 +0200

    Added removal of enemy entity in stop method

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
@@ -3,6 +3,7 @@ package dk.sdu.mmmi.cbse.enemy;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.enemy.CommonEnemyEntity;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 /**
@@ -30,7 +31,7 @@ public class EnemyPlugin implements IGamePluginService {
      */
     @Override
     public void stop(GameData gameData, World world) {
-
+        world.removeEntity(enemyShip);
     }
 
     /**
